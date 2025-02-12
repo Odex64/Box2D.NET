6e9076d877a36391a9d5cf7da78a67ffe96a4f1c
Odex64 <carmine.pietroluongo@outlook.it>
2025-02-12 13:06:46 +0100

    `MathHelper` and `Utils` classes

---
+++ Box2D.NET/Common/MathHelper.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Numerics;
+
+namespace Box2D.NET.Common;
+
+/// <summary>
+/// Provides mathematical helper methods for numeric operations.
+/// </summary>
+/// <remarks>
+/// This class contains utility methods for mathematical operations
+/// that can work with various numeric types.
+/// </remarks>
+public static class MathHelper
+{
+    /// <summary>
+    /// Determines if a number is a power of two.
+    /// </summary>
+    /// <typeparam name="T">The numeric type, must implement <see cref="INumber{T}"/>.</typeparam>
+    /// <param name="value">The number to check.</param>
+    /// <returns>True if the number is a power of two; otherwise, false.</returns>
+    /// <remarks>
+    /// A number is a power of two if it has exactly one bit set in its binary representation.
+    /// This method uses a bitwise operation to check if the number is a power of two.
+    /// </remarks>
+    public static bool IsPowerOfTwo<T>(T value) where T : INumber<T>
+    {
+        // Handle the special case where value is zero (not a power of two)
+        if (value == T.Zero)
+            return false;
+
+        // Convert the value to an integer for bitwise operations
+        int intValue = Convert.ToInt32(value);
+
+        // A number is a power of two if it has exactly one bit set in its binary representation
+        // This is true if (value & (value - 1)) == 0
+        return (intValue & (intValue - 1)) == 0;
+    }
+
+    /// <summary>
+    /// Returns the next power of two greater than or equal to the given number.
+    /// </summary>
+    /// <typeparam name="T">The numeric type, must implement <see cref="INumber{T}"/>.</typeparam>
+    /// <param name="value">The number to find the next power of two for.</param>
+    /// <returns>The next power of two greater than or equal to <paramref name="value"/>.</returns>
+    /// <remarks>
+    /// This method uses a bitwise trick to find the next power of two:
+    /// The method progressively ORs the number with right-shifted versions of itself to fill in all bits 
+    /// to the right of the most significant bit with `1`s, and then adds `1` to find the next power of two.
+    /// If the input number is already a power of two, it will return the same value.
+    /// </remarks>
+    public static T NextPowerOfTwo<T>(T value) where T : INumber<T>
+    {
+        // Handle the special case where value is zero (next power of two is 1)
+        if (value == T.Zero)
+            return T.One;
+
+        // Convert to int for bitwise manipulation
+        int intValue = Convert.ToInt32(value);
+
+        // Use the efficient bitwise trick to find the next power of two
+        int x = intValue;
+        x |= x >> 1;
+        x |= x >> 2;
+        x |= x >> 4;
+        x |= x >> 8;
+        x |= x >> 16;
+
+        // Return the next power of two (x + 1)
+        return (T)Convert.ChangeType(x + 1, typeof(T));
+    }
+}
---
+++ Box2D.NET/Common/Utils.cs
@@ -0,0 +1,28 @@
+namespace Box2D.NET.Common;
+
+/// <summary>
+/// A collection of utility methods for general-purpose operations.
+/// </summary>
+/// <remarks>
+/// This class contains various helper methods, such as the ability to swap values,
+/// which can be useful in a variety of programming scenarios.
+/// </remarks>
+public static class Utils
+{
+    /// <summary>
+    /// Swaps the values of two variables.
+    /// </summary>
+    /// <typeparam name="T">The type of the variables to swap. This can be any type.</typeparam>
+    /// <param name="left">The first value to swap.</param>
+    /// <param name="right">The second value to swap.</param>
+    /// <remarks>
+    /// This method uses tuple deconstruction to swap the values of <paramref name="left"/> 
+    /// and <paramref name="right"/> efficiently. Both values are passed by reference, so
+    /// the changes are reflected outside the method.
+    /// </remarks>
+    public static void Swap<T>(ref T left, ref T right)
+    {
+        // Swap the values of left and right using tuple deconstruction
+        (right, left) = (left, right);
+    }
+}

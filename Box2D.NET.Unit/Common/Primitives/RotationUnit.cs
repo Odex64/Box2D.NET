ba0ebab48b36fa017057d7aac00e20dcaba1cbaf
Odex64 <carmine.pietroluongo@outlook.it>
2025-02-14 19:41:45 +0100

    `Rotation` full test coverage

---
+++ Box2D.NET.Unit/Common/Primitives/RotationUnit.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using Box2D.NET.Common.Primitives;
+using NUnit.Framework;
+
+namespace Box2D.NET.Unit.Common.Primitives;
+
+[TestFixture]
+public class RotationUnit
+{
+    [Test]
+    public void AngleConstructor()
+    {
+        const float angle = MathF.PI / 4f; // 45 degrees
+        Rotation rotation = new Rotation(angle);
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(rotation.Sine, Is.EqualTo(MathF.Sin(angle)).Within(1e-6f));
+            Assert.That(rotation.Cosine, Is.EqualTo(MathF.Cos(angle)).Within(1e-6f));
+        });
+    }
+
+    [Test]
+    public void CosineSineConstructor()
+    {
+        const float sine = 0.6f;
+        const float cosine = 0.8f;
+        Rotation rotation = new Rotation(sine, cosine);
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(rotation.Sine, Is.EqualTo(sine));
+            Assert.That(rotation.Cosine, Is.EqualTo(cosine));
+        });
+    }
+
+    [Test]
+    public void SetAngle()
+    {
+        Rotation rotation = new Rotation(0f);
+        const float angle = MathF.PI / 3f; // 60 degrees
+        rotation.Set(angle);
+
+        Assert.That(rotation, Is.EqualTo(new Rotation(angle)));
+    }
+
+    [Test]
+    public void SetIdentity()
+    {
+        const float angle = MathF.PI / 2f;
+        Rotation rotation = new Rotation(angle);
+        rotation.SetIdentity();
+
+        Assert.That(rotation, Is.EqualTo(new Rotation(0f, 1f)));
+    }
+
+    [Test]
+    public void GetAngle()
+    {
+        const float angle = MathF.PI / 6f; // 30 degrees
+        Rotation rotation = new Rotation(angle);
+
+        Assert.That(rotation.Angle, Is.EqualTo(angle));
+    }
+
+    [Test]
+    public void Multiply()
+    {
+        Rotation rotation1 = new Rotation(MathF.PI / 4f); // 45 degrees
+        Rotation rotation2 = new Rotation(MathF.PI / 6f); // 30 degrees
+        Rotation result = Rotation.Multiply(rotation1, rotation2);
+
+        float expectedSine = MathF.Sin(MathF.PI / 4f + MathF.PI / 6f);
+        float expectedCosine = MathF.Cos(MathF.PI / 4f + MathF.PI / 6f);
+
+        Assert.That(result, Is.EqualTo(new Rotation(expectedSine, expectedCosine)));
+    }
+
+    [Test]
+    public void MultiplyTranspose()
+    {
+        Rotation rotation1 = new Rotation(MathF.PI / 4f); // 45 degrees
+        Rotation rotation2 = new Rotation(MathF.PI / 6f); // 30 degrees
+        Rotation result = Rotation.MultiplyTranspose(rotation1, rotation2);
+
+        float expectedSine = MathF.Sin(MathF.PI / 6f - MathF.PI / 4f);
+        float expectedCosine = MathF.Cos(MathF.PI / 6f - MathF.PI / 4f);
+
+        Assert.That(result, Is.EqualTo(new Rotation(expectedSine, expectedCosine)));
+    }
+
+    [Test]
+    public void MultiplyByVector()
+    {
+        Rotation rotation = new Rotation(MathF.PI / 2f); // 90 degrees
+        Vector2 vector = new Vector2(1f, 0f);
+        Vector2 result = Rotation.Multiply(rotation, vector);
+
+        // Rotating (1, 0) by 90 degrees should result in (0, 1)
+        Assert.That(result, Is.EqualTo(new Vector2(0f, 1f)));
+    }
+
+    [Test]
+    public void MultiplyTransposeByVector()
+    {
+        Rotation rotation = new Rotation(MathF.PI / 2f); // 90 degrees
+        Vector2 vector = new Vector2(0f, 1f);
+        Vector2 result = Rotation.MultiplyTranspose(rotation, vector);
+
+        // Inverse rotating (0, 1) by 90 degrees should result in (1, 0)
+        Assert.That(result, Is.EqualTo(new Vector2(1f, 0f)));
+    }
+}

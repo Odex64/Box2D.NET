d51724a0911fbc409ff7b43374c1d5ef09b86339
Odex64 <carmine.pietroluongo@outlook.it>
2025-02-17 16:12:49 +0100

    Added `GetPointStates`

--- Box2D.NET/Collisions/Manifolds/Manifold.cs
+++ Box2D.NET/Collisions/Manifolds/Manifold.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Linq;
+using Box2D.NET.Collisions.Contacts;
+using Box2D.NET.Collisions.Misc;
 using Box2D.NET.Common;
 using Box2D.NET.Common.Primitives;
 
@@ -62,7 +64,64 @@ public struct Manifold : IEquatable<Manifold>
         Type = type;
     }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Manifold" /> struct with default values.
+    /// </summary>
     public Manifold() => Points = new ManifoldPoint[Constants.MaxManifoldPoints];
+
+    /// <summary>
+    /// Determines the state of contact points between two manifolds.
+    /// </summary>
+    /// <param name="state1">An array to store the state of points from the first manifold.</param>
+    /// <param name="state2">An array to store the state of points from the second manifold.</param>
+    /// <param name="manifold1">The first manifold.</param>
+    /// <param name="manifold2">The second manifold.</param>
+    public static void GetPointStates(PointState[] state1, PointState[] state2, in Manifold manifold1, in Manifold manifold2)
+    {
+        if (state1.Length < Constants.MaxManifoldPoints || state2.Length < Constants.MaxManifoldPoints)
+        {
+            throw new ArgumentException($"The state arrays must have at least {Constants.MaxManifoldPoints} elements.");
+        }
+
+        // Initialize all states to NullState.
+        for (int i = 0; i < Constants.MaxManifoldPoints; ++i)
+        {
+            state1[i] = PointState.NullState;
+            state2[i] = PointState.NullState;
+        }
+
+        // Detect persists and removes for manifold1.
+        for (int i = 0; i < manifold1.Points.Length; ++i)
+        {
+            ContactId id = manifold1.Points[i].Id;
+            state1[i] = PointState.RemoveState;
+
+            for (int j = 0; j < manifold2.Points.Length; ++j)
+            {
+                if (manifold2.Points[j].Id.Key == id.Key)
+                {
+                    state1[i] = PointState.PersistState;
+                    break;
+                }
+            }
+        }
+
+        // Detect persists and adds for manifold2.
+        for (int i = 0; i < manifold2.Points.Length; ++i)
+        {
+            ContactId id = manifold2.Points[i].Id;
+            state2[i] = PointState.AddState;
+
+            for (int j = 0; j < manifold1.Points.Length; ++j)
+            {
+                if (manifold1.Points[j].Id.Key == id.Key)
+                {
+                    state2[i] = PointState.PersistState;
+                    break;
+                }
+            }
+        }
+    }
 
     /// <inheritdoc />
     public bool Equals(Manifold other) =>
